f8ff37c61d5cf2511d6ef74637e57860161e1bd4
Hrayr Derbedrossian <94538722+hrayrdb@users.noreply.github.com>
2024-05-23 16:25:26 +0300

    commit

--- mail.php
+++ mail.php
@@ -2,9 +2,9 @@
 
 if ( $_POST ) {
 
-	$frm_name  = 'John Doe'; // your name
-	$recepient = 'john@example.com'; // your e-mail
-	$sitename  = 'Rhye HTML5 Template'; // your site name
+	$frm_name  = 'AdTempo'; // your name
+	$recepient = 'info@ad-tempo.com'; // your e-mail
+	$sitename  = 'AdTempo'; // your site name
 	$subject   = "New contact from \"$sitename\""; // subject template
 
 	$name  = trim( $_POST['visitor_name'] );
